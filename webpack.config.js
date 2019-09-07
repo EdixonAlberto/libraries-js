4e9c5c59aec8bbd8cbe7ae26f4b81779f381fc67
Edixon Alberto <edixonalbertto@gmail.com>
2019-09-07 05:43:59 -0400

    instalando webpack
    - creando archivos de conf para usar babel y webpack
    - instalando librerias necesarias para implementar webpack y
      convertir codigo de vue
    - moviendo archivos estaticos a dir /public
    - editando package para agregar script para desarrorllo

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: __dirname + '/public',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: '/node_modules/',
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+};
