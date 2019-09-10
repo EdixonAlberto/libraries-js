380af322aee497b15c481e626897e24a1caaad75
Edixon Alberto <edixonalbertto@gmail.com>
2019-09-10 01:01:21 -0400

    editando conf webpack
    - instalando librerias para extraer archivo estaticos (img css)
    - implementando plugin en webpack
    - editando y agregando script en package para manejar webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,30 @@
+const patch = require('path');
 const { VueLoaderPlugin } = require('vue-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+// TODO: minificar el codigo para produccion
 
 module.exports = {
-    entry: './src/main.js',
+    entry: patch.resolve(__dirname, 'src/main.js'),
     output: {
-        path: __dirname + '/public',
-        filename: 'bundle.js'
+        path: patch.resolve(__dirname, 'dist'),
+        filename: 'static/js/[contenthash].js'
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: '/node_modules/',
-                loader: 'babel-loader'
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            cacheDirectory: true,
+                            // presets: ['@babel/preset-env'] // TODO: probar esto, para eliminar .babelrc
+                        }
+                    }
+                ]
             },
             {
                 test: /\.vue$/,
@@ -21,16 +33,32 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [
-                    'vue-style-loader',
+                    MiniCssExtractPlugin.loader,
+                    // 'vue-style-loader', // TODO: agregar ENV para definir este loader
                     'css-loader'
                 ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'static/img',
+                    name: '[contenthash].[ext]',
+                    publicPath: 'static/img'
+                }
             }
         ]
     },
     plugins: [
         new VueLoaderPlugin(),
         new HtmlWebpackPlugin({
-            template: './src/index.html'
+            template: patch.resolve(__dirname, 'src/index.html'),
+            cache: true
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'static/css/[contenthash].css',
+            chunkFilename: '[id].css',
+            ignoreOrder: false
         })
     ]
 };
