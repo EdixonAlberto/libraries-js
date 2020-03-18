d05562d91280a7813b1589fa843167468677f256
EdixonAlberto <edixonalbertto@gmail.com>
2020-03-18 11:43:02 -0400

    test

--- webpack.config.js
+++ webpack.config.js
@@ -6,59 +6,59 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // TODO: minificar el codigo para produccion
 
 module.exports = {
-    entry: patch.resolve(__dirname, 'src/main.js'),
-    output: {
-        path: patch.resolve(__dirname, 'dist'),
-        filename: 'static/js/[contenthash].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: '/node_modules/',
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            cacheDirectory: true,
-                            // presets: ['@babel/preset-env'] // TODO: probar esto, para eliminar .babelrc
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    // 'vue-style-loader', // TODO: agregar ENV para definir este loader
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                loader: 'file-loader',
-                options: {
-                    outputPath: 'static/img',
-                    name: '[contenthash].[ext]',
-                    publicPath: 'static/img'
-                }
-            }
-        ]
-    },
-    plugins: [
-        new VueLoaderPlugin(),
-        new HtmlWebpackPlugin({
-            template: patch.resolve(__dirname, 'src/index.html'),
-            cache: true
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'static/css/[contenthash].css',
-            chunkFilename: '[id].css',
-            ignoreOrder: false
-        })
-    ]
+	entry: patch.resolve(__dirname, 'src/main.js'),
+	output: {
+		path: patch.resolve(__dirname, 'dist'),
+		filename: 'static/js/[contenthash].js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: '/node_modules/',
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							cacheDirectory: true,
+							// presets: ['@babel/preset-env'] // TODO: probar esto, para eliminar .babelrc
+						}
+					}
+				]
+			},
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader'
+			},
+			{
+				test: /\.css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					// 'vue-style-loader', // TODO: agregar ENV para definir este loader
+					'css-loader'
+				]
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				loader: 'file-loader',
+				options: {
+					outputPath: 'static/img',
+					name: '[contenthash].[ext]',
+					publicPath: 'static/img'
+				}
+			}
+		]
+	},
+	plugins: [
+		new VueLoaderPlugin(),
+		new HtmlWebpackPlugin({
+			template: patch.resolve(__dirname, 'src/index.html'),
+			cache: true
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'static/css/[contenthash].css',
+			chunkFilename: '[id].css',
+			ignoreOrder: false
+		})
+	]
 };
